d970489144db4fbea9417f0a106b352187c86f37
Sam Spackman <sspackman99@gmail.com>
2022-10-26 21:10:27 -0600

    game

--- remote_control.py
+++ remote_control.py
@@ -6,11 +6,11 @@ import time
 
 pygame.init()
 
-# SCREEN_SIZE = (500,500)
+SCREEN_SIZE = (500,500)
 
-# screen = pygame.display.set_mode(SCREEN_SIZE)
+screen = pygame.display.set_mode(SCREEN_SIZE)
 
-# pygame.display.set_caption('RoboCon 2022')
+pygame.display.set_caption('RoboCon 2022')
 
 #Create motor class
 
