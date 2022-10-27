5bb6303d10287d3b92bba54a1db6ad61d77b69f9
Sam Spackman <sspackman99@gmail.com>
2022-10-26 20:42:52 -0600

    fixing game

--- remote_control.py
+++ remote_control.py
@@ -9,7 +9,7 @@ pygame.init()
 
 SCREEN_SIZE = (500,500)
 
-screen = pygame.display.set_mode((SCREEN_SIZE))
+screen = pygame.display.set_mode(SCREEN_SIZE)
 
 pygame.display.set_caption('RoboCon 2022')
 
