e24ca221e69f08dcdb3622399038284589d7db45
Sam Spackman <sspackman99@gmail.com>
2022-10-26 22:09:21 -0600

    changed pins

--- remote_control.py
+++ remote_control.py
@@ -39,10 +39,10 @@ GPIO.setmode(GPIO.BCM)
 #set GPIO Pins
 GPIO_TRIGGER = 16
 GPIO_ECHO = 12
-MOTOR1 = Motor(23,24)
-MOTOR2 = Motor(18,17)
-MOTOR3 = Motor(20,26)
-MOTOR4 = Motor(13,19)
+MOTOR1 = Motor(24,23)
+MOTOR2 = Motor(17,18)
+MOTOR3 = Motor(26,20)
+MOTOR4 = Motor(19,13)
 
 motors = [MOTOR1, MOTOR2, MOTOR3, MOTOR4]
 
